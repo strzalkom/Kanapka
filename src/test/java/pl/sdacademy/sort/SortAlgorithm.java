634b1ba44711c85eee79e87765aece73fb851c1f
RENT-Komputer\RENT <damianszkudlarek@gmail.com>
2017-05-11 20:49:00 +0200

    Zaimplementuj pseudokod bubbleSort

---
+++ src/test/java/pl/sdacademy/sort/SortAlgorithm.java
@@ -0,0 +1,35 @@
+package pl.sdacademy.sort;
+
+public class SortAlgorithm implements ISortAlgorithm {
+
+    public SortAlgorithm() {
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void sort(int[] array, boolean ascending) {
+	// TODO Zaimplementuj ponizszy pseudokod
+//	procedure BubbleSort( var a: array of TItemBubbleSort );
+//	var
+//	  n, newn, i:integer;
+//	begin
+//	  n := high( a );
+//	  repeat
+//	    newn := 0;
+//	    for i := 1 to n   do
+//	      begin
+//	        if a[ i - 1 ] > a[ i ] then
+//	          begin
+//	            swap( a[ i - 1 ], a[ i ]);
+//	            newn := i ;
+//	          end;
+//	      end ;
+//	    n := newn;
+//	  until n = 0;
+//	end;
+//	 
+//	end.
+
+    }
+
+}
