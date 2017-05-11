d339ce87d9e8dd9673b2346c9b8a706613c49bad
RENT-Komputer\RENT <damianszkudlarek@gmail.com>
2017-05-11 18:50:15 +0200

    test shouldSortArrayAscendingByDefault()

--- src/test/java/pl/sdacademy/sort/ISortAlgorithmTest.java
+++ src/test/java/pl/sdacademy/sort/ISortAlgorithmTest.java
@@ -9,8 +9,37 @@ public class ISortAlgorithmTest {
     @Test
     public void shouldSortArrayAscendingByDefault() {
 	// given
+	ISortAlgorithm sortAlgorithm = null;
+	int[] tableToSort = { 1, 5, 87, 2, 4 };
+	int[] sortedTable = { 1, 2, 4, 5, 87 };
 
 	// when
+	sortAlgorithm.sort(tableToSort);
+
+	// then
+	boolean isAllElementEqual = true;
+	int i = 0;
+	while (isAllElementEqual) {
+	    isAllElementEqual = tableToSort[i] == sortedTable[i];
+	    i++;
+	    if (!isAllElementEqual) {
+		break;
+
+	    }
+	}
+
+	assertTrue(isAllElementEqual);
+    }
+
+    @Test
+    public void shouldSortArrayDependingOnTheOrder() {
+	// given
+	ISortAlgorithm sortAlgorithm = null;
+	int[] tableToSort = { 1, 5, 87, 2, 4 };
+	int[] sortedTable = {};
+
+	// when
+	sortAlgorithm.sort(tableToSort);
 
 	// then
 	assertTrue(false);
