a6487c66b3cb3a9fae8f21b5b617d425d6ec20aa
RENT-Komputer\RENT <damianszkudlarek@gmail.com>
2017-05-11 18:21:16 +0200

    ISortAlgorithm test

---
+++ src/test/java/pl/sdacademy/sort/ISortAlgorithmTest.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.sort;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ISortAlgorithmTest {
+
+    @Test
+    public void shouldSortArrayAscendingByDefault() {
+	// given
+
+	// when
+
+	// then
+	assertTrue(false);
+    }
+
+}
