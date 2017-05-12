ca152b54b58529d8a725b63b7c3ef968ead8f093
RENT-Komputer\RENT <damianszkudlarek@gmail.com>
2017-05-12 17:27:54 +0200

    Jak zacząć i implementować bubbleSort ?
    Przeczytaj komentarz w SortAlgorithm.java

--- src/main/java/pl/sdacademy/sort/SortAlgorithm.java
+++ src/main/java/pl/sdacademy/sort/SortAlgorithm.java
@@ -6,29 +6,43 @@ public class SortAlgorithm implements ISortAlgorithm {
 	// TODO Auto-generated constructor stub
     }
 
+    /**
+     * Jak testować implementacje ?</br>
+     * https://drive.google.com/file/d/0B4cxGqCXZKnKdEpMbVExR3lTeDg/view?usp=
+     * sharing wejsc w powyzszy link</br>
+     * testować na przykladzie z obrazka</br>
+     * wypisywac krok po kroku kolejne iteracje petli</br>
+     * i sprawdzac czy sa jak na obrazku. </br>
+     * </br>
+     * Po wykonaniu implementacji testujemy. </br>
+     * Możemy testować w trakcie implementacji</br>
+     * Implementacja będzie zakończona,</br>
+     * gdy wszystkie testy będą zakończone sukcesem.
+     */
     @Override
     public void sort(int[] array, boolean ascending) {
+
 	// TODO Zaimplementuj ponizszy pseudokod
-//	procedure BubbleSort( var a: array of TItemBubbleSort );
-//	var
-//	  n, newn, i:integer;
-//	begin
-//	  n := high( a );
-//	  repeat
-//	    newn := 0;
-//	    for i := 1 to n   do
-//	      begin
-//	        if a[ i - 1 ] > a[ i ] then
-//	          begin
-//	            swap( a[ i - 1 ], a[ i ]);
-//	            newn := i ;
-//	          end;
-//	      end ;
-//	    n := newn;
-//	  until n = 0;
-//	end;
-//	 
-//	end.
+	// procedure BubbleSort( var a: array of TItemBubbleSort );
+	// var
+	// n, newn, i:integer;
+	// begin
+	// n := high( a );
+	// repeat
+	// newn := 0;
+	// for i := 1 to n do
+	// begin
+	// if a[ i - 1 ] > a[ i ] then
+	// begin
+	// swap( a[ i - 1 ], a[ i ]);
+	// newn := i ;
+	// end;
+	// end ;
+	// n := newn;
+	// until n = 0;
+	// end;
+	//
+	// end.
 
     }
 
